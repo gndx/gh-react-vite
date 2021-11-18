95de5fc5a30667c32c86461ba5fe169eaf2b8d84
Oscar Barajas Tavares <oscarbarajastavares@macbook.local>
2021-11-18 17:00:03 -0500

    Feature 1

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import App from './App'
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode
+  document.getElementById('roots')
 )
